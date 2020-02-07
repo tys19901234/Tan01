05189a71fc0c13a0f560131101b0d4d8e684a9bb
Administrator <Administrator@192.168.0.104>
2020-02-07 14:18:45 +0800

    541

---
+++ jt/src/jt/com/Tan.java
@@ -0,0 +1,12 @@
+package jt.com;
+
+import java.util.Date;
+
+
+public class Tan {
+  public static void main(String[] args) {
+	
+		 Date date = new Date();
+    	 System.out.println(date);
+     }
+}
